fa09224bda2ba24ef7c9859cd4fa678a3f52c1c3
Andy Luhrs <aluhrs@outlook.com>
2023-05-11 21:35:08 -0700

    Add other options

--- invokeai/app/invocations/dynamic_prompt.py
+++ invokeai/app/invocations/dynamic_prompt.py
@@ -1,10 +1,15 @@
-from dynamicprompts.generators import RandomPromptGenerator
-from dynamicprompts.wildcards import WildcardManager
 from typing import Literal
 from pydantic import Field
 from .baseinvocation import BaseInvocation, InvocationContext
 from pathlib import Path
 from .prompt import PromptOutput
+
+
+from dynamicprompts.generators import RandomPromptGenerator
+from dynamicprompts.wildcards import WildcardManager
+from dynamicprompts.generators.magicprompt import MagicPromptGenerator
+from dynamicprompts.generators.feelinglucky import FeelingLuckyGenerator
+from dynamicprompts.generators.attentiongenerator import AttentionGenerator
 
 # TODO: Magic Prompts
     # Magic Prompts Length
@@ -26,15 +31,28 @@ class DynamicPromptInvocation(BaseInvocation):
     type: Literal["dynamic_prompt"] = "dynamic_prompt"
     prompt: str = Field(default=None, description="The input prompt")
     feeling_lucky: bool = Field(default=False, description="I'm feeling lucky")
-    attention_grabber: bool = Field(default=False, description="Attention Grabber")
+    #attention_grabber: bool = Field(default=False, description="Attention Grabber")
     magic_prompts: bool = Field(default=False, description="Magic Prompts")
-    magic_prompts_length: int = Field(default=100, description="Magic Prompts Length")
-    magic_prompts_creativity: float = Field(default=0.5,gt=0.0,lt=1.0, description="Magic Prompts Creativity")
+    #magic_prompts_length: int = Field(default=100, description="Magic Prompts Length")
+    #magic_prompts_creativity: float = Field(default=0.5,gt=0.0,lt=1.0, description="Magic Prompts Creativity")
     #fmt: on
 
     def invoke(self, context: InvocationContext) -> PromptOutput:
         wm = WildcardManager(
             Path("D:\\StableDiffusion\\InvokeAI\\invokeai\\app\\invocations\\collections"))
         generator = RandomPromptGenerator(wildcard_manager=wm)
-        thing = generator.generate(self.prompt, num_images=4)
+
+        #TODO: Figure out how to get this loaded in model manager?
+        ##TODO: PArams
+        if self.magic_prompts:
+            generator = MagicPromptGenerator(generator, device=0) # device = 0 for CUDA or -1 for CPU
+            thing = generator.generate(self.prompt, num_images=1)
+        elif self.feeling_lucky:
+            generator = FeelingLuckyGenerator(generator)
+            thing = generator.generate(self.prompt, num_prompts=1)
+        #elif self.attention_grabber:
+        #    generator = AttentionGenerator(generator)
+        #    thing = generator.generate(self.prompt, num_prompts=1)  
+        else:
+            thing = generator.generate(self.prompt, num_images=1)
         return PromptOutput(prompt=thing[0])
